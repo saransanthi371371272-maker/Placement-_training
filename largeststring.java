421a478fb67ec80bcf5054d2acb29427e74a1587
saransanthi371371272-maker <saransanthi371371272@gmail.com>
2025-08-29 14:18:56 +0530

    Create largeststring.java

---
+++ largeststring.java
@@ -0,0 +1,13 @@
+class Str {
+    public static void main(String[] args){
+
+        String s1 = "abcd";
+        System.out.println(s1.length());
+
+        String s2 = "";
+        System.out.println(s2.length());
+
+        String s3 = "a";
+        System.out.println(s3.length());
+    }
+}
