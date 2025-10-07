3d46dadb7288900d8c848146f3160931d4247d68
saransanthi371371272-maker <saransanthi371371272@gmail.com>
2025-10-07 06:53:13 +0530

    Create Bfs.py

---
+++ Bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=" ")
+            visited.add(vertex)
+            queue.extend(neighbor for neighbor in graph[vertex] if neighbor not in visited)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+bfs(graph, 'A')
